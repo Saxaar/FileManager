8523992494e00552ab7ec9a34930f56240d998a8
Saxaar <kuzmiku99@gmail.com>
2023-01-22 22:10:32 +0700

    add view files in table

--- src/main/java/com/filemanager/java/manager/Controllers/Controller.java
+++ src/main/java/com/filemanager/java/manager/Controllers/Controller.java
@@ -2,16 +2,23 @@ package com.filemanager.java.manager.Controllers;
 
 import com.filemanager.java.manager.model.FileInfo;
 import javafx.application.Platform;
+import javafx.beans.property.SimpleObjectProperty;
 import javafx.beans.property.SimpleStringProperty;
+import javafx.beans.value.ObservableValue;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
-import javafx.scene.control.TableColumn;
-import javafx.scene.control.TableView;
+import javafx.scene.control.*;
+import javafx.util.Callback;
 
 
+import java.io.IOException;
 import java.net.URL;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 import java.util.ResourceBundle;
+import java.util.stream.Collectors;
 
 public class Controller implements Initializable {
     @FXML
@@ -23,10 +30,56 @@ public class Controller implements Initializable {
 
     @Override
     public void initialize(URL url, ResourceBundle resourceBundle) {
+
+        //File Type Column
         TableColumn<FileInfo,String> fileTypeColumn = new TableColumn<>();
         fileTypeColumn.setCellValueFactory(param -> new SimpleStringProperty(param.getValue().getType().getName()));
         fileTypeColumn.setPrefWidth(24);
 
-        filesTable.getColumns().add(fileTypeColumn);
+        //File Name Column
+        TableColumn<FileInfo,String> fileNameColumn = new TableColumn<>("Name");
+        fileNameColumn.setCellValueFactory(param -> new SimpleStringProperty(param.getValue().getName()));
+        fileNameColumn.setPrefWidth(240);
+
+        //File size
+        TableColumn<FileInfo,Long> fileSizeColumn = new TableColumn<>("Size");
+        fileSizeColumn.setCellValueFactory(param -> new SimpleObjectProperty<>(param.getValue().getSize()));
+        fileSizeColumn.setPrefWidth(140);
+        fileSizeColumn.setCellFactory(column -> {
+            return new TableCell<FileInfo,Long>() {
+                @Override
+                protected void updateItem(Long item, boolean empty) {
+                    super.updateItem(item, empty);
+                    if(item == null || empty) {
+                        setText(null);
+                        setStyle("");
+                    } else {
+                        String text = String.format("%,d bytes" , item);
+                        if(item == -1L ) {
+                           text = "[DIR]";
+                        }
+                        setText(text);
+
+                    }
+                }
+            };
+        });
+
+
+        filesTable.getColumns().addAll(fileTypeColumn,fileNameColumn,fileSizeColumn);
+        filesTable.getSortOrder().add(fileTypeColumn);
+        updateList(Paths.get("."));
+
+    }
+
+    public void updateList(Path path) {
+        try {
+            filesTable.getItems().clear();
+            filesTable.getItems().addAll(Files.list(path).map(FileInfo::new).collect(Collectors.toList()));
+            filesTable.sort();
+        } catch (IOException e) {
+            Alert alert = new Alert(Alert.AlertType.WARNING,"Unable to read", ButtonType.OK);
+            alert.showAndWait();
+        }
     }
 }
